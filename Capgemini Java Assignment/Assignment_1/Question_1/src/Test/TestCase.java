aaf4038bcf61a11dd2296eda57a5f602d23498b2
Manav Verma <vmanav1999@gmail.com>
2021-04-04 13:08:11 +0530

    Updated TestCase.java

--- Capgemini Java Assignment/Assignment_1/Question_1/src/Test/TestCase.java
+++ Capgemini Java Assignment/Assignment_1/Question_1/src/Test/TestCase.java
@@ -9,32 +9,32 @@ import org.junit.jupiter.api.*;
 
 public class TestCase {
 
-    @Test
+    @org.junit.jupiter.api.Test
     public void testcase_1()
     {
         assertEquals(Main.checkArmstrong(153),"a Armstrong Number");
     }
-    @Test
+    @org.junit.jupiter.api.Test
     public void testcase_2()
     {
         assertEquals(Main.checkArmstrong(371),"a Armstrong Number");
     }
-    @Test
+    @org.junit.jupiter.api.Test
     public void testcase_3()
     {
         assertEquals(Main.checkArmstrong(273),"Not a Armstrong Number");
     }
-    @Test
+    @org.junit.jupiter.api.Test
     public void testcase_4()
     {
         assertEquals(Main.checkArmstrong(1966),"Not a Armstrong Number");
     }
-    @Test
+    @org.junit.jupiter.api.Test
     public void testcase_5()
     {
         assertEquals(Main.checkArmstrong(407),"a Armstrong Number");
     }
-    @Test
+    @org.junit.jupiter.api.Test
     public void testcase_6()
     {
         assertEquals(Main.checkArmstrong(370),"a Armstrong Number");

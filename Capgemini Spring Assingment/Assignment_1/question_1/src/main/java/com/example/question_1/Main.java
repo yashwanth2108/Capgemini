e689492595b11dbe8d324bb8777cf2f1f27535ce
Manav Verma <vmanav1999@gmail.com>
2021-04-07 07:55:03 +0530

    TestCase Added in Question 1

--- Capgemini Spring Assingment/Assignment_1/question_1/src/main/java/com/example/question_1/Main.java
+++ Capgemini Spring Assingment/Assignment_1/question_1/src/main/java/com/example/question_1/Main.java
@@ -6,12 +6,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
+    private static Customer customerObject;
+    public static Customer returnCustomer()
+    {
+        return customerObject;
+    }
+
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
         Customer customer = (Customer) applicationContext.getBean("customer");
         System.out.println();
         System.out.println();
         System.out.println(customer.toString());
+        customerObject=customer;
     }
 
 }
--- Capgemini Spring Assingment/Assignment_1/question_1/src/test/java/com/example/question_1/MainTests.java
+++ Capgemini Spring Assingment/Assignment_1/question_1/src/test/java/com/example/question_1/MainTests.java
@@ -1,13 +1,18 @@
 package com.example.question_1;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MainTests {
 
     @Test
     void contextLoads() {
+        String[] args = new String[0];
+        Address address = new Address("sita ki mathiya , chowk","Shahjahanpur","Uttar Pradesh",242001,"India");
+        Customer customer = new Customer(123123,"Manav Verma",990088977,address);
+        Main.main(args);
+
+        Assertions.assertEquals(customer.toString(),Main.returnCustomer().toString());
     }
 
 }

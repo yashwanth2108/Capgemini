92f74f706c160a9401c7edbe9666c30a201802b9
vManav123 <vmanav1999@gmail.com>
2021-04-12 09:32:27 +0530

    new added

---
+++ Capgemini Spring JDBC Assignment/RoughWork/employeedatabase/src/main/java/com/example/employeedatabase/EmployeedatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeedatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeedatabaseApplication.class, args);
+    }
+
+}
---
+++ Capgemini Spring JDBC Assignment/RoughWork/employeedatabase/src/test/java/com/example/employeedatabase/EmployeedatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeedatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeedatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
